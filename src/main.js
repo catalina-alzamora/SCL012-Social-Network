a2e022f8323e17961a18fca49ad6e2fbc50331cc
catalina-alzamora <alzamoracatalina@gmail.com>
2020-02-02 00:20:46 -0400

    'eslint'

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
 // Este es el punto de entrada de tu aplicacion
 
 import {
-emailLogin, googleSignIn, register, observer, uploadImgAndText, 
+  emailLogin, googleSignIn, register, uploadImgAndText,
 } from './app.js';
 
-window.onload = initialHash
-
 function initialHash() {
-  window.location.hash = '#login'
+  window.location.hash = '#login';
 }
+
+window.onload = initialHash;
 
 const root = document.getElementById('root');
 const main = document.getElementById('fullMain');
@@ -18,17 +18,16 @@ const btnRegister = document.getElementById('register');
 const sectionPerfil = document.getElementById('userPerfil');
 
 btnLogIn.addEventListener('click', () => {
-  const userID = document.getElementById("emailLogIn").value;
-  const userPass = document.getElementById("passwordLogIn").value;
+  const userID = document.getElementById('emailLogIn').value;
+  const userPass = document.getElementById('passwordLogIn').value;
   emailLogin(userID, userPass);
+});
 
-})
-
-btnGoogle.addEventListener('click', googleSignIn)
+btnGoogle.addEventListener('click', googleSignIn);
 
 btnRegister.addEventListener('click', () => {
-  window.location.hash = '#register'  
-})
+  window.location.hash = '#register';
+});
 
 const registerView = () => {
   main.innerHTML = '';
@@ -40,20 +39,18 @@ const registerView = () => {
       <input id="passwordRegister2"class="inputText" type="password" placeholder="Confirma contraseña">
       <button id="signIn" class="btn">Registrate</button>
       </form>`;
-  
   const btnSignIn = document.getElementById('signIn');
 
-  btnSignIn.addEventListener('click', () =>{
+  btnSignIn.addEventListener('click', () => {
     const emailRegister = document.getElementById('emailRegister').value;
-    console.log(emailRegister)
+    // console.log(emailRegister)
     const passRegister = document.getElementById('passwordRegister').value;
-    console.log(passRegister)
+    // console.log(passRegister)
     register(emailRegister, passRegister);
-  })
-  
+  });
 };
 
-const homeView = () =>{
+const homeView = () => {
   root.innerHTML = '';
   root.innerHTML = `<header class="homeLogo">
     <h1><img src="img/logo-boceto.png" alt=""></h1>
@@ -114,18 +111,17 @@ const homeView = () =>{
           </li>
        </ul>
       </nav>
-    <main id="fullMain">     
+    <main id="homeMain">     
     </main>
     <section id="newPostSection">
     </section>
-    <button id="addPost" class="btnAdd"><a href="#addPost">°</a></button>`
-}
+    <button id="addPost" class="btnAdd"><a href="#addPost">°</a></button>`;
+};
 
 const myWorkasView = () => {
-  let newPostSection = document.getElementById('newPostSection');
-  newPostSection.innerHTML='';
-  sectionPerfil.innerHTML = 
-    `<div class="businessCard">
+  const newPostSection = document.getElementById('newPostSection');
+  newPostSection.innerHTML = '';
+  sectionPerfil.innerHTML = `<div class="businessCard">
     <img src="img/photo-user-tester.png">
     <div class="idUser">
       <h3>Nombre Usuario / Seudónimo</h3>
@@ -137,74 +133,63 @@ const myWorkasView = () => {
       <p>Mi linkedin</p>
     </div>
     </div>`;
-}
+};
 const sectionAddPost = () => {
-  let newPostSection = document.getElementById('newPostSection');
-  newPostSection.innerHTML =  
-  `<div id="allPost" class="post">
+  const newPostSection = document.getElementById('newPostSection');
+  newPostSection.innerHTML = `<div id="allPost" class="post">
   <textarea class="basePost" id="textPost" cols="30" rows="10"></textarea>
   <div class="actionButtons">
     <input id="imgForUp" type="file" class="fileAdd">
     <button id="newPost" class="btn">Publicar</button>    
   </div>
-  </div>`
-  
+  </div>`;
   const btnFile = document.getElementById('imgForUp');
   const btnUpPost = document.getElementById('newPost');
 
   btnUpPost.addEventListener('click', () => {
-    const textValue = document.getElementById('textPost').value
+    const textValue = document.getElementById('textPost').value;
     const imgFile = btnFile.files[0];
     uploadImgAndText(imgFile, textValue);
-    
-
-  })
-
-}
+  });
+};
 const db = firebase.firestore();
 const showUpPost = () => {
-  const homeMain = document.getElementById('homeMain')
+  const homeMain = document.getElementById('homeMain');
   db.collection('Post').onSnapshot((querySnapshot) => {
-  homeMain.innerHTML = '';
-  querySnapshot.forEach((doc) => {
-      console.log(doc.data());
-      homeMain.innerHTML +=
-      `<div class="postDiv">
+    homeMain.innerHTML = '';
+    querySnapshot.forEach((doc) => {
+      // console.log(doc.data());
+      homeMain.innerHTML += `<div class="postDiv">
       <div class="postArea"> ${doc.data().post}</div>
       <div> <img class="images" src=${doc.data().img}></div>
       <button class="btn">Comentarios</button>
       </div>
       <div id="oldComents" class="commentDiv"></div>
       <div id="newComents" class="commentDiv"></div>`;
-    // función para mostrar caja de comentarios
-    function showComment (){
-    console.log('mostrar')
-    }
-    let coment = Array.from(document.querySelectorAll('.commentDiv')).forEach((element) => {
-    element.addEventListener('click', () =>{
-    console.log(element);
-    divPost.innerHTML = '';
-      })
-    })
+      // función para mostrar caja de comentarios
+      /* function showComment() {
+       console.log('mostrar');
+      }
+       let coment = Array.from(document.querySelectorAll('.commentDiv')).forEach((element) => {
+       element.addEventListener('click', () => {
+       console.log(element);
+       divPost.innerHTML = '';
+       });
+       }); */
+    });
   });
-});
-}
+};
 
 window.addEventListener('hashchange', () => {
-  if(window.location.hash === '#home'){
-    homeView()
-    showUpPost()
-    
-  } if(window.location.hash === '#register'){
-    registerView()
-    
-  } if(window.location.hash === '#addPost'){
-    sectionAddPost()    
-    
-  } if(window.location.hash === '#myWorks'){
-    myWorkasView()
-    
-  } if(window.location.hash === '#favorite'){
-    
-  }
-})
+  if (window.location.hash === '#home') {
+    homeView();
+    showUpPost();
+  } if (window.location.hash === '#register') {
+    registerView();
+  } if (window.location.hash === '#addPost') {
+    sectionAddPost();
+  } if (window.location.hash === '#myWorks') {
+    myWorkasView();
+  } // if (window.location.hash === '#favorite') {
+  // }
+});
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,8 +1,16 @@
 // importamos la funcion que vamos a testear
-import { myFunction } from "../src/lib/index";
+/* import { myFunction } from "../src/lib/index";
 
 describe('myFunction', () => {
   it('debería ser una función', () => {
     expect(typeof myFunction).toBe('function');
+  });
+}); */
+
+import { emailLogin /* ,googleSignIn, register, observer, uploadImgAndText, */ } from '../src/app.js';
+
+describe('emailLogin', () => {
+  it('debería ser una función', () => {
+    expect(typeof emailLogin).toBe('function');
   });
 });
